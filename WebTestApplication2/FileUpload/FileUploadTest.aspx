facc6df371bdcc026d944e19e0734a13b2eb5e61
Dallas <dallas2k@163.com>
2016-06-06 17:29:51 +0800

    测试FileUpload控件，未成功

---
+++ WebTestApplication2/FileUpload/FileUploadTest.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUploadTest.aspx.cs" Inherits="WebTestApplication2.FileUpload.FileUploadTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <asp:FileUpload ID="FileUpload1" runat="server" />
+                <asp:Button ID="Add" runat="server" OnClick="Add_Click" Text="添加" />
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ WebTestApplication2/FileUpload/FileUploadTest.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebTestApplication2.FileUpload
+{
+    public partial class FileUploadTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Web.UI.WebControls.FileUpload fu = new System.Web.UI.WebControls.FileUpload();
+            Label l = new Label();
+            l.Text = "tt";
+            form1.Controls.Add(fu);
+            form1.Controls.Add(l);
+        }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            System.Web.UI.WebControls.FileUpload fu = new System.Web.UI.WebControls.FileUpload();
+            Label l = new Label();
+            l.Text = "tt";
+            form1.Controls.Add(fu);
+            form1.Controls.Add(l);
+        }
+    }
+}
